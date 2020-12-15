294ced429169d1feb229d64ec3f58b69814e4fe2
siporqueno <strike1000001@gmail.com>
2020-12-15 13:52:37 +0300

    Added commands to README.md, developed console client to upload, list files locally

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # cloud-storage
+
+upld file_name
+Uploads file named file_name to cloud storage. File name shall not contain spaces.
+
+dnld file_name
+Downloads file named file_name from cloud storage. File name shall not contain spaces.
+
+lslc
+Lists files in the local storage.
+
+lscl
+Lists files in the cloud storage.
+
+rmlc file_name_one file_name_two
+Renames file file_name_one as file_name_two in the local storage
+
+rmcl file_name_one file_name_two
+Renames file file_name_one as file_name_two in the cloud storage
+
+dellc file_name
+Deletes file file_name in the local storage
+
+delcl file_name
+Deletes file file_name in the cloud storage
--- client/src/main/java/com/porejemplo/nube/client/ConsoleClient.java
+++ client/src/main/java/com/porejemplo/nube/client/ConsoleClient.java
@@ -3,36 +3,104 @@ package com.porejemplo.nube.client;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class ConsoleClient {
-    private static Scanner scanner = new Scanner(System.in);
+    private Scanner scanner = new Scanner(System.in);
 
     public ConsoleClient() {
         try (Socket socket = new Socket("localhost", 8189);
              DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-             DataInputStream in = new DataInputStream(socket.getInputStream())) {
-            System.out.println("Welcome to Cloud storage client.");
-            while (true) {
-                System.out.println("Enter a command or press \\q to quit");
-                String response = scanner.next();
-                switch (response) {
-                    case "send":
-                        Path path = Paths.get("client_storage", "1.txt");
-                        sendFile(out, path);
-                        break;
-                    case "\\q":
-                        break;
-                    default:
-                        System.out.println("Such command does not exist.");
-                }
-                if (response.equals("\\q")) break;
-            }
+             DataInputStream in = new DataInputStream(socket.getInputStream());) {
+            scanner.useDelimiter("\\n");
+            useClient(out);
 
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            scanner.close();
+        }
+    }
+
+    private void useClient(DataOutputStream outputStream) throws IOException {
+        System.out.println("Welcome to Cloud storage client.");
+        while (true) {
+            System.out.println("Enter a command or press \\q to quit");
+            String response = scanner.next();
+            String[] respTokens = response.split(" ");
+            switch (respTokens[0]) {
+                case "lslc":
+                    if (respTokens.length > 1) {
+                        System.out.println("Wrong format of the command lslc");
+                        break;
+                    }
+                    Path pathStrorage = Paths.get("client_storage");
+                    Files.list(pathStrorage).forEach((p) -> System.out.println(p.getFileName()));
+                    break;
+                case "lscl":
+                    if (respTokens.length > 1) {
+                        System.out.println("Wrong format of the command lscl");
+                        break;
+                    }
+                    System.out.println("Command lscl is under development. Waiting for Netty.");
+                    break;
+                case "upld":
+                    if (respTokens.length > 2) {
+                        System.out.println("Wrong format of the command upld");
+                        break;
+                    }
+                    Path path = Paths.get("client_storage", respTokens[1]);
+                    if (Files.notExists(path, LinkOption.NOFOLLOW_LINKS)) {
+                        System.out.println("Such file does not exist");
+                        break;
+                    } else {
+                        sendFile(outputStream, path);
+                        break;
+                    }
+                case "dnld":
+                    if (respTokens.length > 2) {
+                        System.out.println("Wrong format of the command dnld");
+                        break;
+                    }
+                    System.out.println("Command dnld is under development. Waiting for Netty.");
+                    break;
+                case "rmlc":
+                    if (respTokens.length > 3) {
+                        System.out.println("Wrong format of the command rmlc");
+                        break;
+                    }
+                    System.out.println("Command rmlc is under development.");
+                    break;
+                case "rmcl":
+                    if (respTokens.length > 3) {
+                        System.out.println("Wrong format of the command rmlc");
+                        break;
+                    }
+                    System.out.println("Command rmcl is under development. Waiting for Netty.");
+                    break;
+                case "dellc":
+                    if (respTokens.length > 2) {
+                        System.out.println("Wrong format of the command dellc");
+                        break;
+                    }
+                    System.out.println("Command dellc is under development.");
+                    break;
+                case "delcl":
+                    if (respTokens.length > 2) {
+                        System.out.println("Wrong format of the command delcl");
+                        break;
+                    }
+                    System.out.println("Command delcl is under development. Waiting for Netty.");
+                    break;
+                case "\\q":
+                    break;
+                default:
+                    System.out.println("Such command does not exist.");
+            }
+            if (response.equals("\\q")) break;
         }
     }
 
